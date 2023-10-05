3af17882fc6fd25198e43d4b5551c533e9cbcafe
Aarush Sinha <90822297+chungimungi@users.noreply.github.com>
2023-10-05 18:06:33 +0530

    Delete scripts directory

--- scripts/eval.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Schedule execution of many runs
-# Run from root folder with: bash scripts/eval.sh
-
-python src/eval.py
--- scripts/train.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Schedule execution of many runs
-# Run from root folder with: bash scripts/train.sh
-
-python src/train.py
